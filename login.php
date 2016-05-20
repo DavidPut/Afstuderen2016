dbf31c70b353a7989478d041ffed822d639008ec
danny van dalen <danny_vandalen@hotmail.com>
2016-05-20 15:22:33 +0200

    Danny van Dalen - commit changes

--- login.php
+++ login.php
@@ -109,7 +109,7 @@ if (isset($_SESSION['email'])) {
             </div>
             
             <div class="row">
-              <div class="col-md-12 col-xs-12">
+              <div class="col-md-4 col-xs-12">
                <button class="btn btn-lg btn-primary btn-block" type="submit" name=submit" value="inloggen">Inloggen</button>
 
               </div>
