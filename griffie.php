86d92b35ec8b2e9cd3568ac144cf1a9ad3080046
danny van dalen <danny_vandalen@hotmail.com>
2016-05-25 11:26:43 +0200

    Danny van Dalen - commit changes

--- griffie.php
+++ griffie.php
@@ -30,116 +30,148 @@
 <div class="container-fluid">
   <form class="form-horizontal" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
 
-  <div class="row">
-    <div class="col-md-8 col-md-offset-2 col-xs-12 ">
-      <div class="page-header">
-        <h2>Nieuwe besluitvorming</h2>
+    <!-- nieuwe besluitvorming -->
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2 col-xs-12 ">
+        <div class="page-header">
+          <h2>Nieuwe besluitvorming</h2>
+        </div>
       </div>
     </div>
-  </div>
-
-  <div class="row">
 
     <div class="row">
+
+      <div class="row">
+          <div class="col-md-8 col-md-offset-2 col-xs-12 ">
+            <div class="form-group">
+              <label for="inputTitle" class="col-sm-2 control-label">Titel</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" placeholder="Titel besluitvorming">
+              </div>
+            </div>
+          </div>
+        </div>
+
+      <div class="row">
         <div class="col-md-8 col-md-offset-2 col-xs-12 ">
           <div class="form-group">
-            <label for="inputTitle" class="col-sm-2 control-label">Titel</label>
+            <label for="inputSummary" class="col-sm-2 control-label">Samenvatting</label>
             <div class="col-sm-10">
-              <input type="text" class="form-control" placeholder="Titel besluitvorming">
+              <textarea class="form-control" name="summary" placeholder="Samenvatting besluitvorming" rows="3"></textarea>
             </div>
           </div>
         </div>
       </div>
 
-    <div class="row">
-      <div class="col-md-8 col-md-offset-2 col-xs-12 ">
-        <div class="form-group">
-          <label for="inputSummary" class="col-sm-2 control-label">Samenvatting</label>
-          <div class="col-sm-10">
-            <textarea class="form-control" name="summary" placeholder="Samenvatting besluitvorming" rows="3"></textarea>
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2 col-xs-12 ">
+          <div class="form-group">
+            <label for="inputAdrescode" class="col-sm-2 control-label">Postcode/adres</label>
+            <div class="col-sm-10">
+              <input class="form-control" type="text" name="adrescode" placeholder="Een postcode of adres"></input>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="row">
-      <div class="col-md-8 col-md-offset-2 col-xs-12 ">
-        <div class="form-group">
-          <label for="inputAdrescode" class="col-sm-2 control-label">Postcode/adres</label>
-          <div class="col-sm-10">
-            <input class="form-control" type="text" name="adrescode" placeholder="Een postcode of adres"></input>
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2 col-xs-12 ">
+          <div class="form-group">
+            <label for="inputTags" class="col-sm-2 control-label">Zoekwoorden</label>
+            <div class="col-sm-10">
+              <input class="form-control" type="text" name="tags" placeholder="Tags, gescheiden door komma's"></input>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="row">
-      <div class="col-md-8 col-md-offset-2 col-xs-12 ">
-        <div class="form-group">
-          <label for="inputTags" class="col-sm-2 control-label">Zoekwoorden</label>
-          <div class="col-sm-10">
-            <input class="form-control" type="text" name="tags" placeholder="Tags, gescheiden door komma's"></input>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-8 col-md-offset-2 col-xs-12 ">
-        <div class="form-group">
-          <label for="inputTags" class="col-sm-2 control-label">Soort</label>
-          <div class="col-sm-10">
-            <label class="checkbox-inline">
-              <input type="checkbox" id="inlineCheckbox1" value="option1"> Fysiek
-            </label>
-            <label class="checkbox-inline">
-              <input type="checkbox" id="inlineCheckbox2" value="option2"> Sociaal
-            </label>
-            <label class="checkbox-inline">
-              <input type="checkbox" id="inlineCheckbox3" value="option3"> Bestuur en middelen
-            </label>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-8 col-md-offset-2 col-xs-12 ">
-        <div class="form-group">
-          <label for="inputTags" class="col-sm-2 control-label">Periode</label>
-          <div class="col-sm-10">
-            <div class="radio">
-              <label>
-                <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
-                Korter dan een jaar
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2 col-xs-12 ">
+          <div class="form-group">
+            <label for="inputTags" class="col-sm-2 control-label">Soort</label>
+            <div class="col-sm-10">
+              <label class="checkbox-inline">
+                <input type="checkbox" id="inlineCheckbox1" value="option1"> Fysiek
               </label>
-            </div>
-            <div class="radio">
-              <label>
-                <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-                Langer dan een jaar, korter dan vijf jaar
+              <label class="checkbox-inline">
+                <input type="checkbox" id="inlineCheckbox2" value="option2"> Sociaal
               </label>
-            </div>
-            <div class="radio">
-              <label>
-                <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
-                Vijf jaar of langer
+              <label class="checkbox-inline">
+                <input type="checkbox" id="inlineCheckbox3" value="option3"> Bestuur en middelen
               </label>
             </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2 col-xs-12 ">
+          <div class="form-group">
+            <label for="inputTags" class="col-sm-2 control-label">Periode</label>
+            <div class="col-sm-10">
+              <div class="radio">
+                <label>
+                  <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
+                  Korter dan een jaar
+                </label>
+              </div>
+              <div class="radio">
+                <label>
+                  <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+                  Langer dan een jaar, korter dan vijf jaar
+                </label>
+              </div>
+              <div class="radio">
+                <label>
+                  <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+                  Vijf jaar of langer
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
 
 
       <div class="row">
-        <div class="col-md-4 col-md-offset-4 col-xs-12">
-          <button class="btn btn-lg btn-primary btn-block" type="submit" name="add" value="toevoegen">Toevoegen</button>
-
+        <div class="col-md-8 col-md-offset-2 col-xs-12 ">
+          <div class="page-header">
+            <h2>Proces toevoegen</h2>
+          </div>
         </div>
       </div>
 
-  </div>
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 col-xs-12 ">
+            <div class="form-group">
+              <label for="inputTitle" class="col-sm-2 control-label">Titel</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" placeholder="Titel besluitvorming">
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 col-xs-12 ">
+            <div class="form-group">
+              <label for="inputSummary" class="col-sm-2 control-label">Samenvatting</label>
+              <div class="col-sm-10">
+                <textarea class="form-control" name="summary" placeholder="Samenvatting besluitvorming" rows="3"></textarea>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4 col-md-offset-4 col-xs-12">
+            <button class="btn btn-lg btn-primary btn-block" type="submit" name="add" value="toevoegen">Toevoegen</button>
+          </div>
+        </div>
+
+    </div>
+
   </form>
 </body>
 
