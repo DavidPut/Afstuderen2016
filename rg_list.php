6d59daf2cc33b2868ac87567cc4a22f205a047d4
danny van dalen <danny_vandalen@hotmail.com>
2016-05-25 14:25:44 +0200

    Danny van Dalen - commit changes

--- rg_list.php
+++ rg_list.php
@@ -13,7 +13,7 @@ $griffie = 1;
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="stylesheet" type="text/css" href="bootstrap/style/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="style/backend_css22.css"/>
+  <link rel="stylesheet" type="text/css" href="style/backend_css.css"/>
   <script src="scripts/jquery-1.12.3.min.js"></script>
   <script src="bootstrap/scripts/bootstrap.min.js"></script>
 
