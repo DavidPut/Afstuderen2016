37c0e633111ca6adda33e6cd78fcbcffcbb4f712
danny van dalen <danny_vandalen@hotmail.com>
2016-05-26 01:29:18 +0200

    Danny van Dalen - commit changes

--- rg_list.php
+++ rg_list.php
@@ -36,7 +36,7 @@ $griffie = 1;
 <div class="container-fluid">
   <div class="row">
     <div class="col-md-8 col-md-offset-2 col-xs-12 ">
-      <button class="btn btn-list btn-success pull-right"><i class="fa fa-plus" aria-hidden="true">Toevoegen</i></button>
+      <button data-toggle="tooltip" data-placement="top" title='Besluitvormingsproces toevoegen' class="btn btn-list btn-success pull-right"><i class="fa fa-plus" aria-hidden="true"></i></button>
   <table class="table table-hover table-list">
     <thead>
       <tr>
