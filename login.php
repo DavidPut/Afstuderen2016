2efa872856d9e72b984f509f1aeab286d78a7651
danny van dalen <danny_vandalen@hotmail.com>
2016-05-30 13:05:35 +0200

    Danny van Dalen - commit changes

--- login.php
+++ login.php
@@ -10,15 +10,39 @@ $email 		= "";
 $wachtwoord = "";
 
 //Check the user pressed the submit button
-if(isset($_POST['submit'])) {
+if(isset($_POST['submit']) && isset($_POST['mail']) && isset($_POST['password'])) {
+
+  $form_mail = $_POST['mail'];
+  $form_pass = $_POST['password'];
 
   require_once "database/db_functions.php";
   $db_login = new DB_functions();
-  
-  $mail = $_POST['email'];
-  $password = $_POST['wachtwoord'];
-  
-  $db_login_info = $db_login->login($mail);
+  $db_login_info = $db_login->login($form_mail);
+
+  $database_pass = $db_login_info['password'];
+
+  if (md5($wachtwoord) == $loginWachtwoord) {
+        $login = 1;
+      }
+      else {
+        $login = 0;
+      }
+
+      if ($login == 1) {
+        if ($saveLogin == "on") {
+          //Create longer logged in session
+          $_SESSION['email']= $email;
+        }
+        else if($saveLogin == "") {
+          //Create short logged in session
+          $_SESSION['email']= $email;
+        }
+        header("Location: index.php");
+        mysqli_close($db);
+        exit();
+      }
+
+
   echo $db_login_info["mail"];
   echo $db_login_info["name"];
   echo $db_login_info["id"];
@@ -26,40 +50,6 @@ if(isset($_POST['submit'])) {
   echo $db_login_info["role"];
 
 }
-
-
-
-
-//  $query = "SELECT * FROM inloggegevens WHERE email = '$email'";
-//  $loginInformation = mysqli_query($db, $query) or die('Geen verbinding: ' . mysqli_error($loginInformation));
-//
-//  if ($row = mysqli_fetch_assoc($loginInformation)) {
-//
-//    $loginWachtwoord = $row['wachtwoord'];
-//    if (md5(sha1($wachtwoord)) == $loginWachtwoord) {
-//      $login = 1;
-//    }
-//    else {
-//      $login = 0;
-//    }
-//
-//    if ($login == 1) {
-//      if ($saveLogin == "on") {
-//        //Create longer logged in session
-//        $_SESSION['email']= $email;
-//        $_SESSION['time'] = time();
-//      }
-//      else if($saveLogin == "") {
-//        //Create short logged in session
-//        $_SESSION['email']= $email;
-//        $_SESSION['time'] = time();
-//      }
-//      header("Location: index.php");
-//      mysqli_close($db);
-//      exit();
-//    }
-//  }
-//}
 
 //If there is already a session, go to index and do stuff test test
 if (isset($_SESSION['email'])) {
@@ -104,7 +94,7 @@ if (isset($_SESSION['email'])) {
               <div class="col-md-4 col-xs-12 col-md-offset-4">
                 <div class="input-group">
                   <span class="input-group-addon" id="basic-addon1">E-mailadres</span>
-                  <input type="email" name="email" class="form-control" placeholder="uw e-mailadres" aria-describedby="basic-addon1" autofocus>
+                  <input type="email" name="mail" class="form-control" placeholder="uw e-mailadres" aria-describedby="basic-addon1" autofocus>
                 </div>
               </div>
             </div>
