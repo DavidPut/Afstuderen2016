27cfdb6c6305b2a02b391eeb3e58508e5cf16251
danny van dalen <danny_vandalen@hotmail.com>
2016-05-30 19:27:03 +0200

    Danny van Dalen - commit changes

--- admin/griffie/list.php
+++ admin/griffie/list.php
@@ -18,9 +18,9 @@
         <td>Een titel van een besluitvorming</td>
         <td>
           <p class='text-right'>
-            <a href='http://gemeentedossier.nl/dossier.html' data-toggle=\"tooltip\" data-placement=\"top\" title='Ga naar pagina'><button type=\"button\" class=\"btn btn-list btn-default\"><i class=\"fa fa-long-arrow-right\" aria-hidden=\"true\"></i></button></a>
-            <a href='http://gemeentedossier.nl/griffie.php?action=edit' data-toggle=\"tooltip\" data-placement=\"top\" title='Aanpassen'><button type=\"button\" class=\"btn btn-list btn-default\"><i class=\"fa fa-pencil\" aria-hidden=\"true\"></i></button></a>
-            <a href='http://gemeentedossier.nl/griffie.php?action=delete' data-toggle=\"tooltip\" data-placement=\"top\" title='Verwijderen'><button type=\"button\" class=\"btn btn-list btn-danger\"><i class=\"fa fa-trash-o\" aria-hidden=\"true\"></i></button></a>
+            <button type=\"button\" class=\"btn btn-list btn-default\"><a href='http://gemeentedossier.nl/dossier.html' data-toggle=\"tooltip\" data-placement=\"top\" title='Ga naar pagina'><i class=\"fa fa-long-arrow-right\" aria-hidden=\"true\"></i></a></button>
+            <button type=\"button\" class=\"btn btn-list btn-default\"><a href='http://gemeentedossier.nl/griffie.php?action=edit' data-toggle=\"tooltip\" data-placement=\"top\" title='Aanpassen'><i class=\"fa fa-pencil\" aria-hidden=\"true\"></i></a></button>
+            <button type=\"button\" class=\"btn btn-list btn-danger\"><a href='http://gemeentedossier.nl/griffie.php?action=delete' data-toggle=\"tooltip\" data-placement=\"top\" title='Verwijderen'><i class=\"fa fa-trash-o\" aria-hidden=\"true\"></i></a></button>
           </p>
         </td>
       </tr>"; } ?>
