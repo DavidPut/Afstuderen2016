6ea8a237d0c624b6cbf44fc88bdb1e0eb30b58d4
danny van dalen <danny_vandalen@hotmail.com>
2016-05-30 15:49:54 +0200

    Danny van Dalen - commit changes

--- logout.php
+++ logout.php
@@ -1,10 +1,20 @@
 <?php
 
-if ( isset( $_COOKIE[session_name()] ) )
-  setcookie( session_name(),"", time()-3600,"/" );
-//clear session from globals
+session_start
+// Unset all of the session variables.
 $_SESSION = array();
-//clear session from disk
+
+// If it's desired to kill the session, also delete the session cookie.
+// Note: This will destroy the session, and not just the session data!
+if (ini_get("session.use_cookies")) {
+  $params = session_get_cookie_params();
+  setcookie(session_name(), '', time() - 42000,
+    $params["path"], $params["domain"],
+    $params["secure"], $params["httponly"]
+  );
+}
+
+// Finally, destroy the session.
 session_destroy();
 
 
