e1dfc7c53e03a84f07b78483b5bfc84fcde55a0a
David <david_put@hotmail.com>
2016-05-30 10:33:12 +0200

    Code update

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,8 @@ $( document ).ready(function() {
 });
 
 function initMap() {
+    //ToDo: zoom map with user location
+
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
