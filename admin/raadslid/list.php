2f038c9e9b7cb6531d594eab02a14ebafdc5dd2d
danny van dalen <danny_vandalen@hotmail.com>
2016-05-30 19:51:23 +0200

    Danny van Dalen - commit changes

--- admin/raadslid/list.php
+++ admin/raadslid/list.php
@@ -22,8 +22,8 @@
         <td>2</td>
         <td>ja</td>
         <td>
-          <button type=\"button\" class=\"btn btn-list btn-default\"  data-toggle=\"tooltip\" data-placement=\"top\" title='Ga naar pagina'><i class=\"fa fa-long-arrow-right small-icon\" aria-hidden=\"true\"><a href='http://gemeentedossier.nl/dossier.html'></a></i></button>
-          <button type=\"button\" class=\"btn btn-list btn-default\" data-toggle=\"tooltip\" data-placement=\"top\" title='Aanpassen'><i class=\"fa fa-pencil small-icon\" aria-hidden=\"true\"><a href='http://gemeentedossier.nl/griffie.php?action=edit'></a></i></button>
+          <button type=\"button\" class=\"btn btn-list btn-default\"  data-toggle=\"tooltip\" data-placement=\"top\" title='Ga naar pagina'><i class=\"fa fa-long-arrow-right small-icon\" aria-hidden=\"true\"><a href='dossier.html'></a></i></button>
+          <button type=\"button\" class=\"btn btn-list btn-default\" data-toggle=\"tooltip\" data-placement=\"top\" title='Aanpassen'><i class=\"fa fa-pencil small-icon\" aria-hidden=\"true\"><a href='/griffie.php?action=edit'></a></i></button>
           <button type=\"button\" class=\"btn btn-list btn-default\" data-toggle=\"tooltip\" data-placement=\"top\" title='Delen'><i class=\"fa fa-share-alt small-icon\" aria-hidden=\"true\"><a href='https://www.facebook.com/share.php?u=gemeentedossier.nl/griffie.php?action=new&title=test' target=\"blank\"></a></i></button>
         </td>
       </tr>";
