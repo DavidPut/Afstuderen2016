2af7c4b54a93be71f673cf1d36a4d978b484b911
danny van dalen <danny_vandalen@hotmail.com>
2016-05-30 15:17:46 +0200

    Danny van Dalen - commit changes

--- logout.php
+++ logout.php
@@ -3,9 +3,8 @@
 session_start();
 
 //If there is already a session, go to index and do stuff test testtest
-if (isset($_SESSION['mail'])) {
-  session_destroy();
-}
+session_destroy();
+
 
 header("Location: login");
 exit();
