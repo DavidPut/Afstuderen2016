dfefe668e54955570b40c9c8707bb7f88bb484fb
danny van dalen <danny_vandalen@hotmail.com>
2016-05-30 11:44:24 +0200

    Danny van Dalen - commit changes

---
+++ database/config.php
@@ -0,0 +1,5 @@
+<?php
+define('DB_USER', "user"); // db user
+define('DB_PASSWORD', "password"); // db password
+define('DB_DATABASE', "database"); // database name
+define('DB_SERVER', "localhost"); // db server/ host name. -
