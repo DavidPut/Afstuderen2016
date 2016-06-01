48cfe3d5f487c855e35eceae2ee39b958b7753ca
danny van dalen <danny_vandalen@hotmail.com>
2016-06-01 09:57:05 +0200

    Danny van Dalen - commit changes

--- griffie.php
+++ griffie.php
@@ -72,6 +72,35 @@ if (isset($_SESSION['mail'])) {
 <script>
   $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
+
+    $( document ).ready(function() {
+      console.log( "ready!" );
+
+      var nowTemp = new Date();
+      var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+
+      var checkin = $('#dp3').datepicker({
+        onRender: function(date) {
+          return date.valueOf() < now.valueOf() ? 'disabled' : '';
+        }
+      }).on('changeDate', function(ev) {
+        if (ev.date.valueOf() > checkout.date.valueOf()) {
+          var newDate = new Date(ev.date)
+          newDate.setDate(newDate.getDate() + 1);
+          checkout.setValue(newDate);
+        }
+        checkin.hide();
+        $('#dp3')[0].focus();
+      }).data('datepicker');
+      var checkout = $('#dp3').datepicker({
+        onRender: function(date) {
+          return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+        }
+      }).on('changeDate', function(ev) {
+        checkout.hide();
+      }).data('datepicker');
+    });
+
   });
 </script>
 
