75a8356f177f6edc716953fd369884b410f05d0a
danny van dalen <danny_vandalen@hotmail.com>
2016-06-01 11:52:19 +0200

    Danny van Dalen - commit changes

--- admin/griffie/edit.php
+++ admin/griffie/edit.php
@@ -234,9 +234,7 @@
       <div class="form-group" id="datepicker-edit">
         <label for="inputDateExtra" class="col-sm-2 control-label">Datum</label>
         <div class="col-sm-10 input-group date">
-          <input type="text" class="form-control">
-            <span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i>
-          </span>
+          <input type="text" class="form-control"><span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span></input>
         </div>
       </div>
     </div>
