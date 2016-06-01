b811497f683b31ec3a6d70c1b2c0283817093615
danny van dalen <danny_vandalen@hotmail.com>
2016-06-01 14:51:44 +0200

    Danny van Dalen - commit changes

--- griffie.php
+++ griffie.php
@@ -53,9 +53,9 @@ if (isset($_SESSION['mail'])) {
     </div>
     <div class="collapse navbar-collapse navbar-menubuilder">
       <ul class="nav navbar-nav navbar-right">
-        <li><p class="navbar-text navbar-right"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> <a href="logout.php" class="navbar-link"><?php echo $_SESSION['name']; ?></a></p>
+        <li><p class="navbar-text navbar-right"><i class="fa fa-user" aria-hidden="true"></i><a href="logout.php" class="navbar-link"><?php echo $_SESSION['name']; ?></a></p>
         </li>
-        <li><p class="navbar-text navbar-right"><span class="fa fa-sign-out" aria-hidden="true"></span><a href="logout.php" class="navbar-link">Uitloggen</a></p>
+        <li><p class="navbar-text navbar-right"><i class="fa fa-sign-out" aria-hidden="true"></i><a href="logout.php" class="navbar-link">Uitloggen</a></p>
         </li>
       </ul>
     </div>
