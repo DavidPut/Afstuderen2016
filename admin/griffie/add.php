0d271b1a22040cd561492aa5b16dcc37f85097b6
danny van dalen <danny_vandalen@hotmail.com>
2016-06-02 10:22:49 +0200

    Danny van Dalen - commit changes

--- admin/griffie/add.php
+++ admin/griffie/add.php
@@ -120,48 +120,7 @@
 
   <div class="row">
     <div class="col-md-10 col-md-offset-1 col-xs-12 ">
-
-
-      <div class = "panel panel-default">
-        <div class = "panel-heading">
-          <h3 class = "panel-title">Nieuwe besluitvorming</h3>
-        </div>
-        <div class = "panel-body">
-
-          <div class="row">
-            <div class="col-md-12 col-xs-12 ">
-              <div class="form-group">
-                <label for="inputTitleProces" class="col-sm-2 control-label">Titel</label>
-                <div class="col-sm-10">
-                  <input type="text" name="titleProces" class="form-control" placeholder="Titel proces">
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-md-12 col-xs-12 ">
-              <div class="form-group">
-                <label for="inputSummaryProces" class="col-sm-2 control-label">Samenvatting proces</label>
-                <div class="col-sm-10">
-                  <textarea class="form-control" name="summaryProces" placeholder="Samenvatting besluitvorming" rows="6"></textarea>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-md-12 col-xs-12 ">
-              <div class="form-group">
-                <label for="inputSummaryProces" class="col-sm-2 control-label">Bestanden</label>
-                <div class="col-sm-10">
-                  <input class="form-control" type="file" name="inputFile"></input>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div class="alert alert-info" role="alert">Er kunnen nog geen besluiten worden toegevoegd.</div>
     </div>
   </div>
 
@@ -172,20 +131,20 @@
             <div class = "panel-heading">
                 <h3 class = "panel-title">Bestaande besluitvorming</h3>
             </div>
+
             <div class = "panel-body">
+              <div class="row">
+                <div class="col-md-12 col-xs-12 ">
+                  <div class="form-group">
+                  <label for="inputTitleProces" class="col-sm-2 control-label">Titel</label>
+                    <div class="col-sm-10">
+                      <input type="text" name="titleProces" class="form-control" value="Commissievergaderinng">
+                    </div>
+                  </div>
+                </div>
+              </div>
 
-                <div class="row">
-      <div class="col-md-12 col-xs-12 ">
-        <div class="form-group">
-          <label for="inputTitleProces" class="col-sm-2 control-label">Titel</label>
-          <div class="col-sm-10">
-            <input type="text" name="titleProces" class="form-control" value="Commissievergaderinng">
-          </div>
-        </div>
-      </div>
-    </div>
-
-                <div class="row">
+              <div class="row">
       <div class="col-md-12 col-xs-12 ">
         <div class="form-group">
           <label for="inputSummaryProces" class="col-sm-2 control-label">Samenvatting proces</label>
