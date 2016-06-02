7f44347fc0e0d1267a126b73ae01b2c940cf8220
danny van dalen <danny_vandalen@hotmail.com>
2016-06-02 15:21:09 +0200

    Danny van Dalen - commit changes

--- admin/griffie/besluitvorming/bvlist.php
+++ admin/griffie/besluitvorming/bvlist.php
@@ -28,18 +28,5 @@ if($action == 'edit'){
           </p>
         </td>
       </tr>"; } ?>
-
-
-<!--    echo "-->
-<!--      <tr>-->
-<!--        <td>" . $x . "</td>-->
-<!--        <td>Een titel van een besluitvorming</td>-->
-<!--        <td>-->
-<!--          <p class='text-right'>";-->
-<!--            if($action == 'edit'){ echo "<a class=\"btn btn-default \" data-toggle=\"tooltip\" data-placement=\"top\" title='Aanpassen' href=\"besluitvorming/edit/\"><i class=\"fa fa-pencil fa-fw\"></i></a>";}-->
-<!--            echo "<a class=\"btn btn-danger ".(($action == 'delete')?"disabled":'')." \" data-toggle=\"tooltip\" data-placement=\"top\" title='Verwijderen' href=\"besluitvorming/delete/\"><i class=\"fa fa-trash-o fa-fw\"></i></a>";-->
-<!--          echo "</p>-->
-<!--        </td>-->
-<!--      </tr>";} ?>-->
   </tbody>
 </table>
