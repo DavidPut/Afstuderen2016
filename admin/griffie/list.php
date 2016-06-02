228438a24421b8a1e5457cab0621d161cdef3166
danny van dalen <danny_vandalen@hotmail.com>
2016-06-02 12:33:45 +0200

    Danny van Dalen - commit changes

--- admin/griffie/list.php
+++ admin/griffie/list.php
@@ -14,13 +14,13 @@
       <?php foreach ($db_getList_info as $db_getList_info_item) {
         echo " 
       <tr>
-        <td>" . $db_getList_info_item['id'] . "</td>
-        <td>Een titel van een besluitvorming</td>
+        <td>".$db_getList_info_item['id']."</td>
+        <td>".$db_getList_info_item['title']."</td>
         <td>
           <p class='text-right'>
-            <a class=\"btn btn-default\" data-toggle=\"tooltip\" data-placement=\"top\" title='Ga naar pagina' href=\"dossier.html\"><i class=\"fa fa-long-arrow-right fa-fw\"></i></a>
-            <a class=\"btn btn-default\" data-toggle=\"tooltip\" data-placement=\"top\" title='Aanpassen' href=\"griffie/edit\"><i class=\"fa fa-pencil fa-fw\"></i></a>
-            <a class=\"btn btn-danger\" data-toggle=\"tooltip\" data-placement=\"top\" title='Verwijderen' href=\"griffie/delete\"><i class=\"fa fa-trash-o fa-fw\"></i></a>
+            <a class=\"btn btn-default\" data-toggle=\"tooltip\" data-placement=\"top\" title='Ga naar pagina' href=\"dossier.html?id=".$db_getList_info_item['id']."\"><i class=\"fa fa-long-arrow-right fa-fw\"></i></a>
+            <a class=\"btn btn-default\" data-toggle=\"tooltip\" data-placement=\"top\" title='Aanpassen' href=\"griffie/edit/".$db_getList_info_item['id']."\"><i class=\"fa fa-pencil fa-fw\"></i></a>
+            <a class=\"btn btn-danger\" data-toggle=\"tooltip\" data-placement=\"top\" title='Verwijderen' href=\"griffie/delete/".$db_getList_info_item['id']."\"><i class=\"fa fa-trash-o fa-fw\"></i></a>
           </p>
         </td>
       </tr>"; } ?>
