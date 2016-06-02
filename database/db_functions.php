ff42145e5ce4216f3e17063856a1e24ee08a5e52
danny van dalen <danny_vandalen@hotmail.com>
2016-06-02 12:12:17 +0200

    Danny van Dalen - commit changes

--- database/db_functions.php
+++ database/db_functions.php
@@ -37,6 +37,7 @@ class DB_functions
   
   public function griffieList(){
     $result = mysqli_query($this->db->connect(), "SELECT * FROM process") or die(mysqli_error($this->db->connect()));
+    echo "wajo";
     $no_of_rows = mysqli_num_rows($result);
     if ($no_of_rows > 0) {
       return $result;
