d5258beb7e490973dbbc82f3a72f5c5b800bc794
danny van dalen <danny_vandalen@hotmail.com>
2016-06-02 12:24:21 +0200

    Danny van Dalen - commit changes

--- griffie.php
+++ griffie.php
@@ -65,7 +65,7 @@ if(isset($_POST['BVPadd'])) {
 }
 
 if($action == null || $action == 'list') {
-  require_once "database/db_functions.php";
+  require_once "database/db_functions.php"; //test
   $db_getList = new DB_functions();
   $db_getList_info = $db_getList->griffieList();
   print_r($db_getList_info);
