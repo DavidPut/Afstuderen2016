00aafe4bb7755d189a0894346cdc6a385cc11989
danny van dalen <danny_vandalen@hotmail.com>
2016-06-02 16:52:11 +0200

    Danny van Dalen - commit changes

--- formactions.php
+++ formactions.php
@@ -1,5 +1,36 @@
 <?php
 
+if(isset($_POST['login'])) {
+  if (isset($_POST['mail'])) {
+    if (isset($_POST['password'])) {
+
+      $form_mail = $_POST['mail'];
+      $form_pass = $_POST['password'];
+
+      require_once "database/db_functions.php";
+      $db_login = new DB_functions();
+      $db_login_info = $db_login->login($form_mail);
+
+      $database_pass = $db_login_info['password'];
+
+      if (md5($form_pass) == $database_pass) {
+        $_SESSION['mail'] = $db_login_info['mail'];
+        $_SESSION['name'] = $db_login_info["name"];
+        $_SESSION['role'] = $db_login_info["role"];
+        $_SESSION['uid'] = $db_login_info["id"];
+
+        header("location: login");
+        exit();
+      } else {
+        // verkeerde wachtwoord
+        header("Location: login");
+        exit();
+      }
+    }
+  }
+}
+
+
 // Nieuwe besluitvormingsproces
 if(isset($_POST['BVPadd'])) {
   if(isset($_POST['BVPaddTitle'])) {
@@ -51,35 +82,4 @@ if(isset($_POST['BVPadd'])) {
     }
   }
 }
-
-if(isset($_POST['login'])) {
-  if (isset($_POST['mail'])) {
-    if (isset($_POST['password'])) {
-
-      $form_mail = $_POST['mail'];
-      $form_pass = $_POST['password'];
-
-      require_once "database/db_functions.php";
-      $db_login = new DB_functions();
-      $db_login_info = $db_login->login($form_mail);
-
-      $database_pass = $db_login_info['password'];
-
-      if (md5($form_pass) == $database_pass) {
-        $_SESSION['mail'] = $db_login_info['mail'];
-        $_SESSION['name'] = $db_login_info["name"];
-        $_SESSION['role'] = $db_login_info["role"];
-        $_SESSION['uid'] = $db_login_info["id"];
-        
-        header("location: login");
-        exit();
-      } else {
-        // verkeerde wachtwoord
-        header("Location: login");
-        exit();
-      }
-    }
-  }
-}
-
 ?>
