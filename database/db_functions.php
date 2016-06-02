debb9e65799251ac86b37b461f030602222de43b
danny van dalen <danny_vandalen@hotmail.com>
2016-06-02 12:07:28 +0200

    Danny van Dalen - commit changes

--- database/db_functions.php
+++ database/db_functions.php
@@ -39,7 +39,7 @@ class DB_functions
     $result = mysqli_query($this->db->connect(), "SELECT * FROM process") or die(mysqli_error($this->db->connect()));
     $no_of_rows = mysqli_num_rows($result);
     if ($no_of_rows > 0) {
-      return $result->fetch_assoc();
+      return $result;
       $this->db->close();
     } else {
       return false;
