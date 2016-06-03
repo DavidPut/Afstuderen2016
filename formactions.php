35382bf7ca19413930e3207ea8305829b9984a03
danny van dalen <danny_vandalen@hotmail.com>
2016-06-03 14:52:10 +0200

    Danny van Dalen - commit changes

--- formactions.php
+++ formactions.php
@@ -187,7 +187,7 @@ if(isset($_POST['BVadd'])) {
 
         require_once "database/db_functions.php";
         $db_addBVPush = new DB_functions();
-        $db_addBVPush_info = $db_addPush->griffieBVAdd($pid, $BVtitle, $BVsummary);
+        $db_addBVPush_info = $db_addBVPush->griffieBVAdd($pid, $BVtitle, $BVsummary);
 
         unset($_SESSION['Callback']);
         unset($_SESSION['POST']);
