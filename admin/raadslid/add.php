af93bdfd3737cf8c86df2bc1312149e34d6fb267
danny van dalen <danny_vandalen@hotmail.com>
2016-06-28 17:35:31 +0200

    Danny van Dalen - commit changes

--- admin/raadslid/add.php
+++ admin/raadslid/add.php
@@ -3,65 +3,7 @@
   <form class="form-horizontal" action="formactions.php" method="POST">
     <input type="hidden" name="pid" value="<?php echo $db_getItem_info['id']; ?>">
 
-    <!-- nieuwe besluitvorming -->
-    <div class="row">
-      <div class="col-md-12 col-xs-12 ">
-        <div class="page-header">
-          <div class="pull-left">
-            <h3>Besluitvormingsproces <span class="label label-primary"><i class="fa fa-pencil small-icon" aria-hidden="true"></i></span> <a class="btn btn-default" href="dossier.php?id=<?php echo $db_getItem_info['id']; ?>">naar pagina</a></h3>
-          </div>
-          <div class="pull-right">
-            <a class="btn btn-default" href="griffie.php">annuleren</a>
-            <button class="btn btn-list btn-primary" type="submit" name ="BVPedit" value="toevoegen">aanpassen</button>
-          </div>
-          <div class="clearfix"></div>
-        </div>
-      </div>
-    </div>
 
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1 col-xs-12 ">
-        <div class="form-group">
-          <label for="inputTitle" class="col-sm-2 control-label">Titel</label>
-          <div class="col-sm-10">
-            <input type="text" name="BVPeditTitle"class="form-control" placeholder="Titel besluitvorming" value="<?php if($_SESSION['Callback'] == true){echo $_POST['BVPeditTitle'];} else {echo $db_getItem_info['title'];} ?>">
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1 col-xs-12 ">
-        <div class="form-group">
-          <label for="inputSummary" class="col-sm-2 control-label">Samenvatting</label>
-          <div class="col-sm-10">
-            <textarea class="form-control" name="BVPeditSummary" placeholder="Samenvatting besluitvorming" rows="3"><?php if($_SESSION['Callback'] == true){echo $_POST['BVPeditSummary'];} else {echo $db_getItem_info['summary'];} ?></textarea>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1 col-xs-12 ">
-        <div class="form-group">
-          <label for="inputAdrescode" class="col-sm-2 control-label">Postcode/adres</label>
-          <div class="col-sm-10">
-            <input class="form-control" type="text" name="BVPeditLocation" placeholder="Een postcode of adres" value="<?php echo $db_getItem_info['location']; ?>"></input>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1 col-xs-12 ">
-        <div class="form-group">
-          <label for="inputTags" class="col-sm-2 control-label">Zoekwoorden</label>
-          <div class="col-sm-10">
-            <input class="form-control" type="text" name="BVPeditTags" placeholder="Tags, gescheiden door komma's" value="<?php echo $db_getItem_info['tags']; ?>"></input>
-          </div>
-        </div>
-      </div>
-    </div>
 
     <?php
     $db_getItem_info['type'];
@@ -89,29 +31,51 @@
 
     <div class="row">
       <div class="col-md-10 col-md-offset-1 col-xs-12 ">
-        <div class="form-group">
-          <label for="inputTags" class="col-sm-2 control-label">Periode</label>
-          <div class="col-sm-10">
-            <div class="radio">
-              <label>
-                <input type="radio" name="BVPeditPeriod" id="optionsRadios1" value="low" <?php if($db_getItem_info['period'] == 'low'){echo 'checked';}?>>
-                Korter dan een jaar
-              </label>
+
+        <div class = "panel panel-default">
+
+          <div class = "panel-heading">
+            <h3 class = "panel-title">Nieuwe besluitvorming</h3>
+          </div>
+
+          <div class = "panel-body">
+
+            <div class="row">
+              <div class="col-md-12 col-xs-12 ">
+                <div class="form-group">
+                  <label for="inputTitleProces" class="col-sm-2 control-label">Titel</label>
+                  <div class="col-sm-10">
+                    <input type="text" name="BVaddTitle" class="form-control" placeholder="Titel besluit">
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="radio">
-              <label>
-                <input type="radio" name="BVPeditPeriod" id="optionsRadios2" value="mid" <?php if($db_getItem_info['period'] == 'mid'){echo 'checked';}?>>
-                Langer dan een jaar, korter dan vijf jaar
-              </label>
+
+            <div class="row">
+              <div class="col-md-12 col-xs-12 ">
+                <div class="form-group">
+                  <label for="inputSummaryProces" class="col-sm-2 control-label">Samenvatting proces</label>
+                  <div class="col-sm-10">
+                    <textarea class="form-control" name="BVaddSummary" placeholder="Samenvatting besluit" rows="6"></textarea>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="radio">
-              <label>
-                <input type="radio" name="BVPeditPeriod" id="optionsRadios3" value="high" <?php if($db_getItem_info['period'] == 'high'){echo 'checked';}?>>
-                Vijf jaar of langer
-              </label>
+
+            <div class="row">
+              <div class="col-md-12 col-xs-12 ">
+                <div class="form-group">
+                  <label for="inputSummaryProces" class="col-sm-2 control-label">Bestanden</label>
+                  <div class="col-sm-10">
+                    <input class="form-control" type="BVaddfile" name="inputFile"></input>
+                  </div>
+                </div>
+              </div>
             </div>
+
           </div>
         </div>
+
       </div>
     </div>
 
