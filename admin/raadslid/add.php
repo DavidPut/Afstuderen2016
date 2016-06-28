2604cdc1810af11376a5a846ae1d75b9e1898a5c
danny van dalen <danny_vandalen@hotmail.com>
2016-06-28 17:47:23 +0200

    Danny van Dalen - commit changes

--- admin/raadslid/add.php
+++ admin/raadslid/add.php
@@ -8,7 +8,8 @@
           <h3>Besluitvorming <span class="label label-success"><i class="fa fa-plus small-icon" aria-hidden="true"></i></span></h3>
         </div>
         <div class="pull-right">
-          <a class="btn btn-default" href="griffie.php?action=edit&id=<?php echo $id; ?>">annuleren</a>
+
+          <a class="btn btn-default" href="raadslid.php?action=edit&id=<?php echo $id; ?>">annuleren</a>
           <button class="btn btn-list btn-success" type="submit" name ="BVadd" value="toevoegen">toevoegen</button>
         </div>
         <div class="clearfix"></div>
@@ -45,6 +46,14 @@
         </div>
       </div>
     </div>
+
+  <div class="row">
+    <div class="col-md-10 col-md-offset-1 col-xs-12 ">
+      <div class="page-header">
+        <h4>Besluitvorming</h4>
+      </div>
+    </div>
+  </div>
 
     <div class="row">
       <div class="col-md-10 col-md-offset-1 col-xs-12 ">
@@ -97,81 +106,7 @@
     </div>
 
 
-    <!-- bv -->
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1 col-xs-12 ">
-        <div class="page-header">
-          <h4>Besluitvorming</h4>
-        </div>
-      </div>
-    </div>
 
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1 col-xs-12 ">
-        <?php require_once "besluitvorming/bvlist.php"; ?>
-      </div>
-    </div>
-
-
-    <!-- extra gegevens -->
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1 col-xs-12 ">
-        <div class="page-header">
-          <h4>Extra gegevens</h4>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1 col-xs-12 ">
-        <div class="form-group">
-          <label for="inputTitleProces" class="col-sm-2 control-label">Agenda</label>
-          <div class="col-sm-10">
-            <input type="text" name="agendaTitle" class="form-control" placeholder="Titel agenda">
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1 col-xs-12 ">
-        <div class="form-group">
-          <label for="inputDateExtra" class="col-sm-2 control-label">Datum</label>
-          <div class="col-sm-10 date">
-            <input type="text" class="form-control input-group-addon-text" placeholder="dd/mm/jjjj">
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1 col-xs-12 ">
-        <div class="form-group">
-          <label for="inputContact" class="col-sm-2 control-label">Contactgegevens</label>
-          <div class="col-sm-10">
-            <div class="checkbox">
-              <label>
-                <input type="checkbox" name="BVPeditContact[]" value="GEM"> Gemeente contactgegevens
-              </label>
-            </div>
-            <div class="checkbox">
-              <label>
-                <input type="checkbox" name="BVPeditContact[]" value="GRIEF"> Griffie contactgegevens
-              </label>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-4 col-md-offset-4 col-xs-12">
-        <a class="btn btn-lg btn-default text-left" href="griffie.php">annuleren</a>
-        <button class="btn btn-lg btn-primary text-right" type = "submit" name = "BVPedit" value = "aanpassen">aanpassen</button>
-      </div>
-    </div>
-
-  </form>
 
 <?php
 if(isset($_SESSION['Callback'])){
