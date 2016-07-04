3a2adf823e54a5451e828a440f0bf056407c6530
danny van dalen <danny_vandalen@hotmail.com>
2016-07-04 22:47:29 +0200

    Danny van Dalen - commit changes

--- admin/griffie/agenda/adelete.php
+++ admin/griffie/agenda/adelete.php
@@ -72,11 +72,11 @@ if (isset($_SESSION['mail'])) {
       <div class="col-md-12 col-xs-12 ">
         <div class="page-header">
           <div class="pull-left">
-            <h3>Besluitvorming <span class="label label-success"><i class="fa fa-plus small-icon" aria-hidden="true"></i></span></h3>
+            <h3>Besluitvorming <span class="label label-danger"><i class="fa fa-trash-o small-icon" aria-hidden="true"></i></span></h3>
           </div>
           <div class="pull-right">
             <a class="btn btn-default" href="griffie.php?action=edit&id=<?php echo $id; ?>">annuleren</a>
-            <button class="btn btn-list btn-success" type="submit" name ="agendaAdd" value="toevoegen">toevoegen</button>
+            <button class="btn btn-list btn-danger" type="submit" name ="agendaAdd" value="verwijderen">verwijderen</button>
           </div>
           <div class="clearfix"></div>
         </div>
@@ -125,7 +125,7 @@ if (isset($_SESSION['mail'])) {
     <div class="row">
       <div class="col-md-4 col-md-offset-4 col-xs-12">
         <a class="btn btn-lg btn-default text-left" href="griffie.php?action=edit&id=<?php echo $id; ?>">annuleren</a>
-        <button class="btn btn-lg btn-success text-right" type="submit" name="agendaAdd" value="toevoegen">toevoegen</button>
+        <button class="btn btn-lg btn-danger text-right" type="submit" name="agendaDelete" value="verwijderen">verwijderen</button>
       </div>
     </div>
 
