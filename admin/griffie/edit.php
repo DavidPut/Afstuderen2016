25aef647eb53fc58b179fe4d925c5dfdb86720f1
danny van dalen <danny_vandalen@hotmail.com>
2016-07-04 19:07:51 +0200

    Danny van Dalen - commit changes

--- admin/griffie/edit.php
+++ admin/griffie/edit.php
@@ -163,19 +163,29 @@
     </div>
   </div>
 
+  <?php
+  require_once "database/db_functions.php"; //test
+  $db_getList = new DB_functions();
+  $db_getContactItem_info = $db_getList->raadslidContactItem($_GET['id'], $_SESSION['uid']);
+
+
+  ?>
+
   <div class="row">
     <div class="col-md-10 col-md-offset-1 col-xs-12 ">
-      <div class="form-group">
-        <label for="inputContact" class="col-sm-2 control-label">Contactgegevens</label>
+      <div class="form-group <?php if($_SESSION['Callback'] == true){echo "has-error";}?>">
+        <label for="inputTags" class="col-sm-2 control-label">Contactgegevens</label>
         <div class="col-sm-10">
-          <div class="checkbox">
+          <div class="radio">
             <label>
-              <input type="checkbox" name="BVPeditContact[]" value="GEM"> Gemeente contactgegevens
+              <input type="radio" name="BVPaddContact" id="optionsRadios1" value="off" <?php if($db_getContactItem_info == false){echo 'checked';}?>>
+              Griffier contactgegevens niet achterlaten
             </label>
           </div>
-          <div class="checkbox">
+          <div class="radio">
             <label>
-              <input type="checkbox" name="BVPeditContact[]" value="GRIEF"> Griffie contactgegevens
+              <input type="radio" name="BVPaddContact" id="optionsRadios2" value="on" <?php if($db_getContactItem_info == true){echo 'checked';}?>>
+              Griffier contactgegevens achterlaten
             </label>
           </div>
         </div>
