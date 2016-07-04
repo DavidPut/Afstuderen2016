bbbf2782ee2089e13abe2fbaccf018fc61b00f2a
danny van dalen <danny_vandalen@hotmail.com>
2016-07-04 02:40:27 +0200

    Danny van Dalen - commit changes

--- database/db_functions.php
+++ database/db_functions.php
@@ -154,7 +154,7 @@ class DB_functions
 
   // pakt de lijst voor raadsleden met de besluiten of ze al gereageerd hebben op het besluit
   public function raadslidList($pid, $did, $uid){
-    $result = mysqli_query($this->db->connect(), "SELECT * FROM process_opinion WHERE pid = '$pid' AND id = '$did' AND uid ='$uid'") or die(mysqli_error($this->db->connect()));
+    $result = mysqli_query($this->db->connect(), "SELECT * FROM process_opinion WHERE pid = '$pid' AND did = '$did' AND uid ='$uid'") or die(mysqli_error($this->db->connect()));
     $no_of_rows = mysqli_num_rows($result);
     if ($no_of_rows > 0) {
       $row = mysqli_fetch_assoc($result);
