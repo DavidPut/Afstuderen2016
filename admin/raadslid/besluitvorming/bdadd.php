cd4bc641f4e65f041c746a7cde5c21d66015af7e
danny van dalen <danny_vandalen@hotmail.com>
2016-07-04 10:56:50 +0200

    Danny van Dalen - commit changes

--- admin/raadslid/besluitvorming/bdadd.php
+++ admin/raadslid/besluitvorming/bdadd.php
@@ -15,7 +15,10 @@ if (isset($_SESSION['mail'])) {
   exit();
 }
 
-// rare rrors soms
+//database verkrijgen data
+$db_getBVOpinionList_info = $db_getBVList->raadslidList($id, $db_getBVList_info_item['id'], $_SESSION['uid']);
+
+//data opsturen
 
 ?>
 
@@ -99,6 +102,7 @@ if (isset($_SESSION['mail'])) {
                 <div class="form-group">
                   <label for="inputTitleProces" class="col-sm-2 control-label">Titel</label>
                   <div class="col-sm-10">
+                    Het is al geruime tijd een bekend gegeven dat een lezer, tijdens het bekijken van de layout van een pagina, afgeleid wordt door de tekstuele inhoud. Het belangrijke punt van het gebruik van Lorem Ipsum is dat het uit een min of meer normale verdeling van letters bestaat, in tegenstelling tot "Hier uw tekst, hier uw tekst" wat het tot min of meer leesbaar nederlands maakt. Veel desktop publishing pakketten en web pagina editors gebruiken tegenwoordig Lorem Ipsum als hun standaard model tekst, en een zoekopdracht naar "lorem ipsum" ontsluit veel websites die nog in aanbouw zijn. Verscheidene versies hebben zich ontwikkeld in de loop van de jaren, soms per ongeluk soms expres (ingevoegde humor en dergelijke).</p>
                     <input type="text" name="BVaddTitle" class="form-control" placeholder="Titel besluit">
                   </div>
                 </div>
