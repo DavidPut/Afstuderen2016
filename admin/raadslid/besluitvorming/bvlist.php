5de450c1691bca5032889a35e0a3226a0ad3ee94
danny van dalen <danny_vandalen@hotmail.com>
2016-07-04 02:10:00 +0200

    Danny van Dalen - commit changes

--- admin/raadslid/besluitvorming/bvlist.php
+++ admin/raadslid/besluitvorming/bvlist.php
@@ -35,7 +35,7 @@ if ($db_getBVList_info){
           <p class='text-right'>";
             if($db_getBVOpinionList_info){
               foreach ($db_getBVOpinionList_info as $db_getBVOpinionList_info_item) {
-                if($db_getBVOpinionList_info_item['did'] == $db_getBVList_info['id']){
+                if($db_getBVOpinionList_info_item['did'] == $db_getBVList_info_item['id']){
                   echo "".(($action == 'edit')?"<a class=\"btn btn-default\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"Aanpassen\" href=\"admin/raadslid?action=list\"><i class=\"fa fa-pencil fa-fw\"></i></a>":"")."
                     <a class=\"btn btn-danger ".(($action == 'delete')?"disabled":'')." \" data-toggle=\"tooltip\" data-placement=\"top\" title='Verwijderen' href=\"admin/raadslid/besluitvorming/bvdelete.php?id=".$db_getItem_info['id']."&bid=".$db_getBVList_info_item['id']."\"><i class=\"fa fa-trash-o fa-fw\"></i></a>";
                 } else {
